b1e3438c1cc4097756829706b39c9525d55da61e
Sanzhar Shaimbetov <55742668+essancho@users.noreply.github.com>
2021-12-14 18:12:29 +0600

    Setup snowpack, initial setup

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import './App.css';
+
+interface AppProps {}
+
+const App: React.FC<AppProps> = () => {
+  return <div className="App">Let's go</div>;
+};
+
+export default App;
